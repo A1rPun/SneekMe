46d436f9f57ab71c1e78ed38361aacdfcdc70831
Thom Bakker <thombakker@gmail.com>
2015-05-22 11:21:39 +0200

    Sprite class (implementation)

--- js/sprite.js
+++ js/sprite.js
@@ -1,45 +1,58 @@
 ﻿SneekMe.sprite = (function () {
     function sprite(options) {
-		this.x = 0;
-		this.y = 0;
-		this.frameRate = 0;
-		this.frameIndex = 0;
-		//Place properties that can be overridden here
-        if (arguments[0]) for (var prop in arguments[0]) this[prop] = arguments[0][prop];
-		//Place properties that CAN'T be overridden here
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        this.frameIndex = 0;
+        this.frameRate = 0;
+        this.frameCount = 0;
+
+        Object.defineProperty(this, "mappings", {
+            set: function (val) {
+                if (!val) return;
+                if (val.shift || val.hasOwnProperty('x')) {
+                    this._mappings = { standard: val };
+                    this.map = 'standard';
+                } else {
+                    this._mappings = val;
+                }
+            }
+        });
+        //Map all options to object
+        if (arguments[0])
+            for (var prop in arguments[0])
+                this[prop] = arguments[0][prop];
     }
+
     sprite.prototype = {
-        
+        draw: function (context, x, y, width, height) {
+            var currentFrame, currentMap = this._mappings[this.map];
+            if (!currentMap) return;
+            currentFrame = currentMap.shift ? currentMap[this.frameIndex] : currentMap;
+
+            context.drawImage(
+                /* Image */
+                this.image,
+                /* Source */
+                currentFrame.x,
+                currentFrame.y,
+                currentFrame.width,
+                currentFrame.height,
+                /* Destination */
+                x || this.x,
+                y || this.y,
+                width || this.width,
+                height || this.height
+            );
+
+            this.frameCount++;
+            if (this.frameCount > this.frameRate) {
+                this.frameCount = 0;
+                this.frameIndex++;
+                if (this.frameIndex >= currentMap.length) this.frameIndex = 0;
+            }
+        }
     };
     return sprite;
 })();
-/*
-var sprite = new SneekMe.sprite({
-    x: 200,
-    y: 100,
-    image: imageObj,
-    animation: 'standing',
-    animations: {//object or array
-      standing: [
-        // x, y, width, height (6 frames)
-        0, 0, 49, 109,
-        52, 0, 49, 109,
-        105, 0, 49, 109,
-        158, 0, 49, 109,
-        210, 0, 49, 109,
-        262, 0, 49, 109
-      ],
-      kicking: [
-        // x, y, width, height (6 frames)
-        0, 109, 45, 98,
-        45, 109, 45, 98,
-        95, 109, 63, 98,
-        156, 109, 70, 98,
-        229, 109, 60, 98,
-        287, 109, 41, 98
-      ]          
-    },
-    frameRate: 7,
-    frameIndex: 0
-  });
-*/
